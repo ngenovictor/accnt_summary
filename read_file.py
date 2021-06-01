6dba9e8f99335ad1c440c86b92d350945cece438
Victor Ngeno <ngenovictor321@gmail.com>
2021-06-01 19:12:17 +0300

    add placeholder code for kcb

--- read_file.py
+++ read_file.py
@@ -5,7 +5,9 @@ import pikepdf
 import os
 import pandas as pd
 
-SUPPORTED_FILE_TYPES = ['safaricom']
+import typing
+
+SUPPORTED_FILE_TYPES = ['safaricom', 'kcb']
 
 
 def parse_safaricom_statement(password, file_path):
@@ -37,9 +39,15 @@ def parse_safaricom_statement(password, file_path):
         
     return all_sum
 
+
+def parse_kcb_statement(file_path: str) -> typing.Dict:
+    return dict()
+
 def parse_account_statement(file_type, password, file_path):
     if file_type == 'safaricom':
         return parse_safaricom_statement(password, file_path)
+    elif file_type == 'kcb':
+        return parse_kcb_statement(file_path)
     else:
         raise NotImplementedError()
         
@@ -54,7 +62,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Script to read statement pdf files and give a summary of totals per month.")
     parser.add_argument("--type", choices=SUPPORTED_FILE_TYPES, help=f"Source of the file. Supported are : {SUPPORTED_FILE_TYPES}", required=True)
     parser.add_argument("--password", help="Required for encrypted files. Especially safcom files.")
-    parser.add_argument("--file_path", help="Path to the pdf file to scan", type=lambda x: is_pdf_file(parser, x)  ,required=True)
+    parser.add_argument("--file_path", '-f', help="Path to the pdf file to scan", type=lambda x: is_pdf_file(parser, x)  ,required=True)
     args = parser.parse_args()
     for month, values in parse_account_statement(args.type, args.password, args.file_path).items():
         print(f"{month} ==> {values}")
